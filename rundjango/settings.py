15e931a5d65e6b560c772e9139065e2bfe7e834b
Visrozar <elvisrozarioefr@gmail.com>
2017-08-08 16:54:45 +0530

    Debug mode enabled

--- rundjango/settings.py
+++ rundjango/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-PROJECT_DIR=os.path.dirname(__file__)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-844_0&gajhmpxn+=3kj5bq3kex%e)=%ftb!@v40+20e12t11w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['45.63.31.168']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,6 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_DIR,'home/static/')
+# STATIC_ROOT = os.path.join(BASE_DIR,'home/static/')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = ( os.path.join(PROJECT_DIR,'static/'),)
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "home/static"),
+# ]
