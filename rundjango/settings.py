35de2410ccd772550011b5407761bc5c03f3ed82
Visrozar <elvisrozarioefr@gmail.com>
2017-08-08 16:20:31 +0530

    Static URL

--- rundjango/settings.py
+++ rundjango/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/static/'
 STATIC_ROOT = "/home/static"
