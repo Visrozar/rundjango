7d8b59b7f8128c310319bc856fbebe2b38c6152e
Visrozar <elvisrozarioefr@gmail.com>
2017-08-08 16:03:52 +0530

    Debug disabled

--- rundjango/settings.py
+++ rundjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-844_0&gajhmpxn+=3kj5bq3kex%e)=%ftb!@v40+20e12t11w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
