fb4529b684bf4a9cda398535ecba04ca7ce8782b
isaac-nadar <isaac.v.nadar@gmail.com>
2017-09-07 13:28:01 +0530

    #1

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    title = models.CharField(max_length=200)
+    company_id = models.AutoField(primary_key=True)
+    def __str__(self):
+        return self.title
